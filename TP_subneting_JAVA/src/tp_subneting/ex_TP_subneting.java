b2ba57fb05c340d9e00d5923d6eeab17d4a0f09f
bassarou <36897493+bassarou120@users.noreply.github.com>
2021-05-03 17:32:04 +0100

    Add files via upload

---
+++ TP_subneting_JAVA/src/tp_subneting/ex_TP_subneting.java
@@ -0,0 +1,70 @@
+//
+//package tp_subneting;
+//
+//import java.awt.FlowLayout;
+//import javax.swing.JButton;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//
+// 
+//
+///**
+// *
+// * @author bassarou Yacoubou
+// */
+//public class ex_TP_subneting extends JFrame {
+//    
+//    
+//  public  ex_TP_subneting(){
+//        
+//      super("TP SUBNETING ESM");
+//      
+//      this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//      this.setVisible(true);
+//      this.setSize(700,700);
+//     this.setLocationRelativeTo(null);
+//     
+//     JPanel contentPane =(JPanel) this.getContentPane();
+//     
+//     contentPane.setLayout(new FlowLayout());
+//     
+//     contentPane.add(new JButton("taper"));
+//       contentPane.add(new JButton("taperfsfsdsdsd"));
+//         contentPane.add(new JButton("taperdssdsdsd"));
+//     
+//     
+//      
+//     
+//    
+//     
+//       
+//                
+//    }
+//    
+//    
+//   
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//        
+//        
+//        ex_TP_subneting tp =new ex_TP_subneting();
+//        
+//        
+//        
+//        
+//        
+//   
+// 
+//
+//
+//        
+//        
+//        
+//        
+//    }
+//    
+//}
